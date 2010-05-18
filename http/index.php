6877a56816a7d5a6fd0cb1a3392ca06b2a1b70e1
drn05r@iamvm-turtle.ecs.soton.ac.uk <drn05r@iamvm-turtle.ecs.soton.ac.uk>
2010-05-18 17:12:13 +0100

    Updated footer started to change index page to talk about linked data

--- http/index.php
+++ http/index.php
@@ -1,17 +1,19 @@
 <?php
 	ini_set('include_path','inc/:.');
-	$pagetitle="RDF API";
+	$pagetitle="Linked Data";
 	include('header.inc.php');
 	include('data.inc.php');
 ?>
-  <div style="float: right;">
-    <?php include('login.inc.php'); ?>
-  </div>
-  <h3 style="margin-top: 0;">Welcome to the RDF API for myExperiment</h3>
-  <p>myExperiment's RDF API provides myExperiment data in RDF/XML format.   Please see the <a href="guide">guide</a> explaining how to request RDF for myExperiment.  Some of myExperiment's data is private and is protected using HTTP authentication.  If you request a URI that is private then you will need to provide your myExperiment username and password to prove you have permission to retrieve this RDF.  To save time you can &quot;log in&quot; using the login form to the right.  This will save you having to provide your creditentials for each URI.  It will also allow you to make requests for RDF for all entities of a certain type that you have permission to see.</p>
-
-  <h3>Ontologies</h3>
-  <p>The structure of myExperiment RDF is defined by the <a href="ontologies/">myExperiment ontologies</a>.  This is a set of modules that borrows classes/properties from FOAF, SIOC, Dublin Core, Creative Commons and OAI-ORE, that can be assembled to build a comprehensive specification for the myExperiment data model.</p>
+  <h3 style="margin-top: 0;">Welcome to myExperiment Linked Data</h3>
+  <p><a href="http://linkeddata.org/">Linked Data</a> is a way of interconnecting data published on the web that has not been previously linked.  myExperiment is a publishers of such data, making its public data available on the web in three different formats:
+    <ul>
+      <li>HTML web pages. E.g  <a href="http://www.myexperiment.org/workflows/16.html">http://www.myexperiment.org/workflows/16.html</a><li>
+      <li>REST API XML. E.g.  <a href="http://www.myexperiment.org/workflows/16.xml">http://www.myexperiment.org/workflows/16.xml</a></li>
+      <li>RDF/XML. E.g.  <a href="http://www.myexperiment.org/workflows/16.rdf">http://www.myexperiment.org/workflows/16.rdf</a></li>
+    </ul>
+  <p>
+  <h3>Ontology</h3>
+  <p>The structure of myExperiment RDF is defined by the <a href="ontologies/">myExperiment Ontology</a>.  This is a set of modules that borrows classes/properties from FOAF, SIOC, Dublin Core, Creative Commons and OAI-ORE, that can be assembled to build a comprehensive specification for the myExperiment data model.</p>
 
   <h3>SPARQL Endpoint</h3>
   <p>All myExperiment's public RDF data can queried using the query language SPARQL at <a href="sparql">myExperiment's SPARQL Endpoint</a>.</p>
@@ -19,4 +21,5 @@
   <h3>OAI-ORE Export</h3>
   <p>myExperiment Packs and Experiments can be exported as OAI-ORE Resource Maps.  Like RDF export private entities can be accessed using HTTP authentication.  A guide for how to export OAI-ORE Resource Maps can be found <a href="ORE">here</a>.</p>
 
+ <br/>
 <?php include('footer.inc.php'); ?>
