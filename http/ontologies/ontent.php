5bbeabe7ed9549d9b00ac63c9d93ce522ee963fa
drn05r@iamvm-turtle.ecs.soton.ac.uk <drn05r@iamvm-turtle.ecs.soton.ac.uk>
2010-06-21 18:46:47 +0100

    Make ontent more robust

--- http/ontologies/ontent.php
+++ http/ontologies/ontent.php
@@ -5,17 +5,21 @@
         $ontology="$ontopath$_GET[ontology]/";
         $lines = file($ontology);
         $l=0;
-        while (strpos($lines[$l],'rdf:about="'.$_GET['entity'].'"')==0){
+	$match = 'rdf:about="'.$_GET['entity'].'"';
+        while (strpos($lines[$l],$match)==0 and $l<sizeof($lines)){
                 $l++;   
         }
         $entity=str_replace($_GET['entity'],$ontology.$_GET['entity'],$lines[$l]);
         $l++;
-        while (strlen(trim($lines[$l]))>0){
+        while (strlen(trim($lines[$l]))>0 and $l<sizeof($lines)){
                 $entity.=$lines[$l];
                 $l++;
         }
-        header('Content-type: application/rdf+xml');
-        echo pageheader();
-        echo $entity;
-        echo pagefooter(); 
+	if ($entity){
+  		header('Content-type: application/rdf+xml');
+        	echo pageheader();
+        	echo $entity;
+        	echo pagefooter(); 
+	}
+	else header("HTTP/1.1 404 Not Found");
 ?> 
