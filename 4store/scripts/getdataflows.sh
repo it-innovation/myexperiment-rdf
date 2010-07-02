e3c42d3ed25637f7234ea9862e42fa0e84020757
drn05r@iamvm-turtle.ecs.soton.ac.uk <drn05r@iamvm-turtle.ecs.soton.ac.uk>
2010-07-02 01:29:57 +0100

    Fixed getdataflows script so it can get settings.sh independent of where it is called from

--- 4store/scripts/getdataflows.sh
+++ 4store/scripts/getdataflows.sh
@@ -2,7 +2,12 @@
 if [ "$1" == "reason" ]; then
   echo "============== `date` =============="
 fi
-source `pwd`/`dirname $BASH_SOURCE`/settings.sh
+bashsource=`dirname $BASH_SOURCE`
+if [ "${bashsource:0:1}" == "/" ]; then
+  source "$bashsource/settings.sh"
+else
+  source "`pwd`/$bashsource/settings.sh"
+fi
 cd $STORE4_PATH/scripts
 /usr/bin/php getNewWorkflowVersions.php | awk -v datapath="$DATA_PATH" -v httpwwwpath="$HTTPWWW_PATH" 'BEGIN{FS=","}{ print " -O " datapath "/dataflows/xml/" $1 " -q " httpwwwpath "/workflow.xml?id=" $2 "&version=" $3 "&elements=components" }' > /tmp/wgets.txt
 exec</tmp/wgets.txt
