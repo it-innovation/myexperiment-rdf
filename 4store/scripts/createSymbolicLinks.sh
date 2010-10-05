4a7b8594f1ee67fc02421ecb3c3f205c7f01637c
drn05r@iamvm-turtle.ecs.soton.ac.uk <drn05r@iamvm-turtle.ecs.soton.ac.uk>
2010-10-05 17:31:19 +0100

    Fixed symbolic links generation script to remove existing links if they already exist before replacing with the new one

--- 4store/scripts/createSymbolicLinks.sh
+++ 4store/scripts/createSymbolicLinks.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 source `dirname $BASH_SOURCE`/settings.sh
+
 if [ -e $LD_PATH/http/myexperiment.rdf.gz ]; then
-	echo "Symbolic Links have already been created!"
-else
-	ln -s $DATA_PATH/$TRIPLESTORE/myexperiment.rdf.gz  $LD_PATH/http/myexperiment.rdf.gz
-	ln -s $DATA_PATH/$TRIPLESTORE/html/spec.html  $LD_PATH/http/ontologies/spec.html
-	ln -s $DATA_PATH/$TRIPLESTORE/$TRIPLESTORE"_reasoned.owl" $LD_PATH/http/ontologies/myexp_reasoned.owl
-	ln -s $DATA_PATH/$TRIPLESTORE/linksets  $LD_PATH/http/linksets
-	ln -s $DATA_PATH/$TRIPLESTORE/void.rdf  $LD_PATH/http/void.rdf
+	rm $LD_PATH/http/myexperiment.rdf.gz
 fi
+ln -s $DATA_PATH/$TRIPLESTORE/myexperiment.rdf.gz  $LD_PATH/http/myexperiment.rdf.gz
+echo "Created Symbolic Link for myexperiment.rdf.gz"
+
+if [ -e $LD_PATH/http/ontologies/spec.html ]; then
+        rm $LD_PATH/http/ontologies/spec.html
+fi
+ln -s $DATA_PATH/$TRIPLESTORE/html/spec.html  $LD_PATH/http/ontologies/spec.html
+echo "Created Symbolic Link for spec.html"
+
+if [ -e $LD_PATH/http/ontologies/myexp_reasoned.owl ]; then
+        rm $LD_PATH/http/ontologies/myexp_reasoned.owl
+fi
+ln -s $DATA_PATH/$TRIPLESTORE/$TRIPLESTORE"_reasoned.owl" $LD_PATH/http/ontologies/myexp_reasoned.owl
+echo "Created Symbolic Link for myexp_reasoned.owl"
+
+if [ -e $LD_PATH/http/linksets ]; then
+        rm $LD_PATH/http/linksets
+fi
+ln -s $DATA_PATH/$TRIPLESTORE/linksets  $LD_PATH/http/linksets
+echo "Created Symbolic Link for linksets folder"
+
+if [ -e $LD_PATH/http/void.rdf ]; then
+	rm $LD_PATH/http/void.rdf
+fi
+ln -s $DATA_PATH/$TRIPLESTORE/void.rdf  $LD_PATH/http/void.rdf
+echo "Created Symbolic Link for void.rdf"
+
 
