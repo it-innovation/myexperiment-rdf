084fe19b3ca480fbf1b24feedc40defb4efb3063
drn <none@none>
2012-03-18 23:57:01 +0000

    more dataflow rdfgen fixes

--- 4store/scripts/generateDataflowRDF.php
+++ 4store/scripts/generateDataflowRDF.php
@@ -32,7 +32,7 @@
 	$filelocdir=$datapath."dataflows/inc/";
 	foreach ($newfiles as $wfvid){
                 if (file_exists($filelocdir.$wfvid)) continue;
-                $sql="select workflow_versions.*, content_types.mime_type from workflow_versions inner join content_types on workflow_versions.content_type_id=content_types.id where id='$wfvid'";
+                $sql="select workflow_versions.*, content_types.mime_type from workflow_versions inner join content_types on workflow_versions.content_type_id=content_types.id where workflow_versions.id='$wfvid'";
                 $wfv=mysql_fetch_assoc(mysql_query($sql));
                 $wget="wget -q -O /tmp/wfvc_$wfvid.xml -o /dev/null '${datauri}workflow.xml?id=$wfv[workflow_id]&versions=$wfv[version]&elements=components'";
                 exec($wget);
